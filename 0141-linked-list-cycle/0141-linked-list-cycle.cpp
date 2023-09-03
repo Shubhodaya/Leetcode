0164be074638bda09407af06bd967cacd7b9b518
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2023-09-03 14:50:17 +0530

    Time: 7 ms (91.25%), Space: 8.1 MB (36.38%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,22 +9,42 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,bool>mp;
-       ListNode* ptr =head;
+    //     unordered_map<ListNode*,bool>mp;
+    //    ListNode* ptr =head;
        
 
 
-       while(ptr){
+    //    while(ptr){
            
-           if(mp[ptr])return true;
-           else{
-               mp[ptr]=true;
-               ptr=ptr->next;
+    //        if(mp[ptr])return true;
+    //        else{
+    //            mp[ptr]=true;
+    //            ptr=ptr->next;
 
-           }
+    //        }
+    //    }
+
+    //    return false;
+
+
+       ListNode* s =head;
+       ListNode* f =head;
+
+if(!head)return false;
+
+       while(f->next && f->next->next){
+
+        s=s->next;
+        f=f->next->next;
+
+        if(s==f)return true;    
+
+
+       
        }
 
        return false;
+        
         
         
     }
