5845df1d388b02c32092d74ab1815f069e805671
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2023-10-19 15:43:48 +0530

    Time: 10 ms (21.69%), Space: 14.5 MB (23.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -7,27 +7,18 @@ class Solution {
     int col =p.second;
     vis[row][col]=1;
 grid[row][col]=colr;
-cout<<row<<" "<<col<<endl;
-    for(int dr=-1;dr<=1;dr++){
-        int nr=row+dr;
-        if(nr>=0 && nr<grid.size()){
-        if(grid[nr][col]==ini && vis[nr][col]==0){
-            // grid[nr][col]=colr;
-            dfs({nr,col},vis,grid,ini,colr);
-        }
-        }
-    }
-    
-    for(int dc=-1;dc<=1;dc++){
-        int nc=col+dc;
-        if(nc>=0 && nc<grid[0].size()){
-        if(grid[row][nc]==ini && vis[row][nc]==0){
-            // grid[row][nc]=colr;
-            dfs({row,nc},vis,grid,ini,colr);
-        }
-        }
-    }
-
+// cout<<row<<" "<<col<<endl;
+  
+  int dr[4]={-1,0,1,0};
+  int dc[4]={0,-1,0,1};
+ 
+ for(int i=0;i<4;i++){
+     int nr= row+dr[i];
+     int nc=col+dc[i];
+     if(nr>=0 && nc>=0 && nr<grid.size() && nc<grid[0].size() && vis[nr][nc]==0 && grid[nr][nc]==ini){
+         dfs({nr,nc},vis,grid,ini,colr);
+     }
+ }
 
 
     }
