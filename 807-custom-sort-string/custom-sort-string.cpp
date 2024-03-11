e89438a544cf4ceaefdd9158b79da57d0c51ae20
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-03-12 00:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.00%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-          std::unordered_map<char, int> charCount;
-        for (char c : order) {
-            charCount[c] = 0;
-        }
-        
-        for (char c : s) {
-            if (charCount.find(c) != charCount.end()) {
-                charCount[c]++;
+        //   std::unordered_map<char, int> charCount;
+        // for (char c : order) {
+        //     charCount[c] = 0;
+        // }
+
+        // for (char c : s) {
+        //     if (charCount.find(c) != charCount.end()) {
+        //         charCount[c]++;
+        //     }
+        // }
+
+        // std::string sortedS;
+        // for (char c : order) {
+        //     sortedS.append(charCount[c], c);
+        // }
+
+        // for (char c : s) {
+        //     if (charCount.find(c) == charCount.end()) {
+        //         sortedS.push_back(c);
+        //     }
+        // }
+
+        // return sortedS;
+        map<char, int> mp;
+        string a = "";
+        for (int i = 0; i < s.size(); i++)
+            mp[s[i]]++;
+
+        for (int i = 0; i < order.size(); i++) {
+            if (mp[order[i]]) {
+                int k = mp[order[i]];
+                while (k--) {
+                    a.push_back(order[i]);
+                    mp[order[i]]--;
+                }
             }
         }
-        
-        std::string sortedS;
-        for (char c : order) {
-            sortedS.append(charCount[c], c);
-        }
-        
-        for (char c : s) {
-            if (charCount.find(c) == charCount.end()) {
-                sortedS.push_back(c);
+
+        for (auto it : mp) {
+            if (it.second) {
+                int k = it.second;
+                while (k--)
+                    a.push_back(it.first);
             }
         }
-        
-        return sortedS;
-//         map<int,int>mp;
-// string a;
-//         for(int i=0;i<s.size();i++)mp[s[i]-'a']++;
-
-//         for(int i=0;i<order.size();i++){
-//             if(mp[order[i]-'a']){
-//                 while(mp[order[i]-'a']--)a += order.substr(i,1); 
-//             }
-//         }
-
-//         for( auto it: mp){
-//             if(it.second){
-//                 int k=it.second;
-//                 while(k--)a += charit.first; 
-//             }
-//         }
-//         return a;
-        
+        return a;
     }
 };
