a68d841f4ea8bebc9e59a34270a5956213421158
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-03-24 18:43:22 +0530

    Time: 114 ms (5.09%) | Memory: 11 MB (30.12%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-          int n = s.size();
-    int ans = 0;        
-    unordered_map<char, int> mp;
-    int i = 0, j = 0;
+int n=s.size(),ans=0;        
+        unordered_map<char,int>mp;int i=0;
+int j=0;
+    while(j<n){
 
-    while (j < n) {
-        mp[s[j]]++;
+ if(!(mp['a']>0 && mp['b']>0 && mp['c']>0))mp[s[j]]++;
 
-        // If the current window contains all three characters 'a', 'b', and 'c'
-        while (mp['a'] > 0 && mp['b'] > 0 && mp['c'] > 0) {
-            // Add the number of substrings ending at index 'j'
-            ans += n - j;
+if(mp['a']>0 && mp['b']>0 && mp['c']>0){
 
-            // Move the left pointer 'i' to the right until the condition is met
-            mp[s[i]]--;
-            i++;
-        }
-        // Move the right pointer 'j' to the right
-        j++;
+ans += n-j;
+mp[s[i]]--;
+i++;
+}
+ if(!(mp['a']>0 && mp['b']>0 && mp['c']>0))j++;
+
     }
-    return ans;
-// int n=s.size(),ans=0;        
-//         unordered_map<char,int>mp;int i=0;
-// int j=0;
-//     while(j<n){
-
-// mp[s[j]]++;
-
-// if(mp['a']>0 && mp['b']>0 && mp['c']>0){
-
-// ans += n-j;
-// mp[s[i]]--;
-// i++;
-// }
-//  if(!(mp['a']>0 && mp['b']>0 && mp['c']>0))j++;
-
-//     }
-// return ans;
+return ans;
     }
 };
