f0d98244fd711b7a627092460d51de84ff9410e7
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-03-26 13:52:18 +0530

    Time: 18 ms (55.06%) | Memory: 21.7 MB (16.99%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -11,49 +11,31 @@
  */
 class Solution {
 public:
-        int count=0;
-   unordered_map<long, int> map;
     int pathSum(TreeNode* root, int targetSum) {
- 
-    function<void(TreeNode*,int,long)>countPathSum = [&](TreeNode* root, int target, long sum){
-        if(!root)
-            return;
-        sum += root->val;        //Path sum from root
-        if(sum == target)
-            count++;
-        if(map.find(sum - target) != map.end())         //checking whether any target sum path present in the path from root to the current node
-            count += map[sum - target];
-        map[sum]++;
-        countPathSum(root->left, target, sum);
-        countPathSum(root->right, target, sum);
-        map[sum]--;      //After visiting the left and right subtree, we have to reduce this path sum count from map since we are leaving this path
-    };
-     countPathSum(root, targetSum, 0);
-     return count;
 
-
-
-//         function<void(TreeNode*,int)>f = [&](TreeNode* root, long sum){
+        int ans=0;
+   unordered_map<long, int> map;
+        function<void(TreeNode*,long)>f = [&](TreeNode* root, long sum){
               
-//               if(!root)return ;
-//               sum +=root->val;
+              if(!root)return ;
+              sum +=root->val;
 
-//               if(sum==targetSum)ans++;
+              if(sum==targetSum)ans++;
 
-//         if(map.find(sum - targetSum) != map.end())         
-//             ans += map[sum - targetSum];
+        if(map.find(sum - targetSum) != map.end())         
+            ans += map[sum - targetSum];
 
-//             map[sum]++;
-//               f(root->left,sum);
-//             //   if(root->left)f(root->left,0);
-//               f(root->right,sum);
-//             //   if(root->right)f(root->right,0);
+            map[sum]++;
+              f(root->left,sum);
+            //   if(root->left)f(root->left,0);
+              f(root->right,sum);
+            //   if(root->right)f(root->right,0);
 
-//             map[sum]--;
+            map[sum]--;
 
-//         };
-// f(root,0);
-// return ans;
+        };
+f(root,0);
+return ans;
 
         
     }
