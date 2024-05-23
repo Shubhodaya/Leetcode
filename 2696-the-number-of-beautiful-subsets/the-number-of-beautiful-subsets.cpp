38ff2862912dd71b780ae605b9637458d6574cc3
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-05-23 13:21:30 +0530

    Time: 272 ms (59.51%) | Memory: 36.9 MB (80.17%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,11 +1,12 @@
 class Solution {
-    int f(int i , int k, vector<int>&st, vector<int>& nums){
+    int f(int i , int k, vector<int>&st, vector<int>& nums,vector<int>&dp){
         int n= nums.size();
 
         if(i==n){
             if(st.size())return 1;
             else return 0;
         }
+        // if(dp[i]!=-1)return dp[i];
 
         int t=0;
         // auto it=st.rbegin();
@@ -15,20 +16,21 @@ class Solution {
 
           }       
 if(fl==0){st.push_back(nums[i]);
-t= f( i+1,k,st,nums);
+t= f( i+1,k,st,nums,dp);
 st.pop_back();
 
 }
 
 
-        int nt=f(i+1,k,st,nums);
+        int nt=f(i+1,k,st,nums,dp);
 
-        return t+nt;
+        return dp[i]= t+nt;
 
     }
 public:
     int beautifulSubsets(vector<int>& nums, int k) {
         vector<int>st;
-        return f(0,k,st,nums);
+        vector<int>dp(nums.size()+1,-1);
+        return f(0,k,st,nums,dp);
     }
 };
