e75c95eb870bbb080363d09353c0eaffba698c1f
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-06-04 14:07:59 +0530

    Time: 0 ms (100%), Space: 9 MB (48.94%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char,int>mp;
-
-        for(int i=0;i< s.size();i++){
-            mp[s[i]]++;
-        }
-
-int ans=0,maxo=0,k=0;
-
+        map<char,int>mp;
+int ans=0;
+        for(int i=0;i<s.size();i++)mp[s[i]]++;
+        int maxi=0;
+int f=0;
         for(auto it: mp){
-
-            ans += (it.second/2)*2;
-            if(it.second%2==1)k=1;
-       
+            if(it.second%2==0)ans +=it.second;
+            else {f=1;ans+=it.second -1;}
         }
-
-        if(k)ans ++;
-
-        return ans;
-
-        
-        
+        return ans+1*(f==1);
     }
 };
