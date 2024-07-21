e18de233eba4885fc92db1151cf7b2847b3486e0
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-07-21 18:49:10 +0530

    Time: 126 ms (100.00%) | Memory: 126.4 MB (100.00%) - LeetSync

---
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long minimumOperations(vector<int>& nums, vector<int>& target) {
+        int n= nums.size();
+        vector<long long>d(n,0);
+
+        for( int i=0;i<n;i++){
+            d[i]=target[i]-nums[i];
+        }
+        long long dif=0,inc=0,dec=0,ans=0;
+        
+        for( int i=0;i<n;i++){
+            dif=d[i];
+            
+            if(dif>0){
+                if(dif>inc)ans+=dif-inc;
+                inc=dif;
+                dec=0;
+            }else if(dif<0){
+                if(dif<dec)ans+= dec-dif;
+                dec=dif;
+                inc=0;
+            }else{
+                inc=0;dec=0;
+            }
+        
+        }
+
+return ans;
+    }
+};
