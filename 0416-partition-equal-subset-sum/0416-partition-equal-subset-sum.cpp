82409c0b9a93155dd8aefb238605243d446141e4
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-08-06 18:28:18 +0530

    Time: 155 ms (53.6%), Space: 99.6 MB (19.64%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    private : bool f(int i,vector<int>& nums,int sum, int s ,vector<vector<int>>&dp,vector<vector<bool>>&dp1){
+    private : bool f(int i,vector<int>& nums,int sum, int s ,vector<vector<int>>&dp){
         int n= nums.size();
         if( i==n)return false;
 
-        if( dp[i][sum]!=-1)return dp1[i][sum];
+        if( dp[i][sum]!=-1)return dp[i][sum];
 
         if(sum==s)return true;
 
 bool t= false;
-        if(nums[i]<=s-sum)t= f(i+1,nums,sum+nums[i],s,dp,dp1);
-        bool nt = f(i+1,nums,sum,s,dp,dp1);
+        if(nums[i]<=s-sum)t= f(i+1,nums,sum+nums[i],s,dp);
+        bool nt = f(i+1,nums,sum,s,dp);
 
-dp[i][sum]=1;
-return dp1[i][sum]=(t||nt);
+// dp[i][sum]=1;
+return dp[i][sum]=(t||nt);
     }
 public:
     bool canPartition(vector<int>& nums) {
@@ -26,8 +26,8 @@ public:
         // int l=0,r=n-1;
         int n= nums.size();
         vector<vector<int>>dp(n+1,vector<int>(s+1,-1));
-        vector<vector<bool>>dp1(n+1,vector<bool>(s+1,false));
-        return f(0,nums,0,s,dp,dp1);
+        // vector<vector<bool>>dp1(n+1,vector<bool>(s+1,false));
+        return f(0,nums,0,s,dp);
 
         
         
