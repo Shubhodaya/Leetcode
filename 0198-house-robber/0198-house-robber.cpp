c142c35d267023e48d4f35485f789a7d49f9921f
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-08-29 02:49:10 +0530

    Time: 0 ms (100%), Space: 9.5 MB (39.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -13,7 +13,17 @@ class Solution {
     }
 public:
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return f(nums.size()-1,nums,dp);
+        vector<int>dp(nums.size()+4,0);
+        // return f(nums.size()-1,nums,dp);
+int n= nums.size();
+        for( int i=3;i<n+3;i++){
+            
+        int t= nums[i-3]+dp[i-2];
+        int nt=dp[i-1];
+
+        dp[i]=max(t,nt);
+        }
+return dp[n+2];
+
     }
 };
