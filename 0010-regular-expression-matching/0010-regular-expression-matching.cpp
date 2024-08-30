11904952e7d928481f43d104eefa5401b3df12d9
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-08-30 20:21:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.5%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,21 +1,23 @@
 class Solution {
 
-    bool f(int i, int j, string&s, string& p){
+    bool f(int i, int j, string&s, string& p,vector<vector<int>>&dp){
 
 int ns=s.size(), np=p.size();
 
-if(i>=ns && j>=np)return true;
+if(i>=ns && j>=np)return 1;
         
-if(j>=p.size())return false;
+if(j>=p.size())return 0;
+
+if(dp[i][j]!=-1)return dp[i][j];
 
 bool match=(i<ns && ((s[i]==p[j])||(p[j]=='.')));
 
 if(j+1<np && p[j+1]=='*')
-return (f(i,j+2,s,p)||(match && f(i+1,j,s,p)));
+return dp[i][j]= (f(i,j+2,s,p,dp)||(match && f(i+1,j,s,p,dp)));
 
-if(match)return f(i+1,j+1,s,p);
+if(match)return dp[i][j]=f(i+1,j+1,s,p,dp);
 
-return false;
+return dp[i][j]=0;
 
         
 
@@ -23,6 +25,7 @@ return false;
     }
 public:
     bool isMatch(string s, string p) {
-        return f(0,0,s,p);
+        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
+        return f(0,0,s,p,dp);
     }
 };
