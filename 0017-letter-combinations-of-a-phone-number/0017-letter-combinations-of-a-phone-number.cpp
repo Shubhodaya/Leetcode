490e30001cc636c7917ccd8954edd7a2969e5784
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-10-01 11:07:51 +0530

    Time: 3 ms (37.91%), Space: 8.6 MB (26.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -23,15 +23,17 @@ public:
         return res;        
     }
 
-    void backtrack(const string& digits, int idx, string comb, vector<string>& res, const unordered_map<char, string>& digitToLetters) {
+    void backtrack(const string& digits, int idx, string comb, vector<string>& res, unordered_map<char, string>& digitToLetters) {
         if (idx == digits.length()) {
             res.push_back(comb);
             return;
         }
         
-        string letters = digitToLetters.at(digits[idx]);
-        for (char letter : letters) {
-            backtrack(digits, idx + 1, comb + letter, res, digitToLetters);
-        }
+   if (digitToLetters.find(digits[idx]) != digitToLetters.end()) {
+    string letters = digitToLetters[digits[idx]];
+    for (char letter : letters) {
+        backtrack(digits, idx + 1, comb + letter, res, digitToLetters);
+    }
+}
     }    
 };
