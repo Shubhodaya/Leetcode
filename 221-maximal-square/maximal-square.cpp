97ae7e92f5c0ab504972767c4a2930bae77207d0
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-10-01 10:35:45 +0530

    Time: 50 ms (95.75%) | Memory: 24.8 MB (37.55%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -28,21 +28,30 @@ public:
     int maximalSquare(vector<vector<char>>& mat) {
         int n= mat.size();
 int m = mat[0].size();
-         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-         f(0,0,mat,dp);
+        //  f(0,0,mat,dp);
 
 int maxi=0;
 
-  for( int i=0;i<n;i++){
-            for( int j=0;j<m;j++)cout<<dp[i][j]<<" ";
-            cout<<endl;
-         }
+//   for( int i=0;i<n;i++){
+//             for( int j=0;j<m;j++)cout<<dp[i][j]<<" ";
+//             cout<<endl;
+//          }
 
 
-         for( int i=0;i<n;i++){
-            for( int j=0;j<m;j++)maxi=max(maxi,dp[i][j]*dp[i][j]);
-         }
+//          for( int i=0;i<n;i++){
+//             for( int j=0;j<m;j++)maxi=max(maxi,dp[i][j]*dp[i][j]);
+//          }
+
+for( int i=n-1;i>=0;i--){
+    for(int j=m-1;j>=0;j--){
+      if(mat[i][j]=='1') dp[i][j]=1+max(0,min(dp[i+1][j+1],min(dp[i+1][j],dp[i][j+1])));
+        maxi=max(maxi,dp[i][j]*dp[i][j]);
+    }
+}
+
+
 
          return maxi;
     
