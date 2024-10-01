47f55ac698d5f4093b03188bd602c12c5205bc83
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-10-01 10:37:55 +0530

    Time: 23 ms (17.8%), Space: 27.6 MB (9.21%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int f(int i, int amt, vector<int>& coins, vector<vector<int>>& dp){
+        if(i>=coins.size()) return 0;
+        if(amt==0) return 1;
+        if(dp[i][amt]!=-1) return dp[i][amt];
+        int np = f(i+1,amt,coins,dp);
+        int p = 0;
+        if(amt>=coins[i]){
+             p = f(i,amt-coins[i],coins,dp);
+        }
+        return dp[i][amt] = p+np;
+    }
+    int change(int amount, vector<int>& coins) {
+        // sort(coins.begin(),coins.end());
+        int n = coins.size();
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        return f(0,amount,coins,dp);
+    }
+};
