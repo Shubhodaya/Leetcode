89e7e2ce9bd13637ec881c34dd86c963ab7f244f
Shubhodaya Sarkar <73129588+Shubhodaya@users.noreply.github.com>
2024-10-01 11:41:39 +0530

    Time: 1245 ms (7.34%), Space: 290 MB (17.95%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>>v;
-        int n= nums.size();
+sort(nums.begin(),nums.end());
+set<vector<int>>st;
+int n= nums.size();
 
-        for( int i=0;i<n;i++){
-            int num=-nums[i];
+for( int i=0;i<n;i++){
 
-            int l=0,r=n-1;
-            while(l<i && i<r){
-// if(l==i)l++;
-// if(r==i)r--;
-                int x= nums[l]+nums[r];
-if(x==num){v.insert({nums[l],nums[i],nums[r]});
-l++;
-r--;
+int num=-nums[i];
+
+int l=0,r=n-1;
+
+while(i>l &&  r>i){
+
+int sum= nums[l]+nums[r];
+
+if(sum==num){st.insert({nums[l],nums[i],nums[r]}); l++;r--;}
+else if(sum<num)l++;
+else r--;
+
+
 }
-                if(x>num)r--;
-                if(x<num) l++;
 
+}
 
-            }
-        }
-        vector<vector<int>>v1(v.begin(),v.end());
-     return v1;   
+vector<vector<int>>v(st.begin(),st.end());
+return v;
     }
+    
 };
